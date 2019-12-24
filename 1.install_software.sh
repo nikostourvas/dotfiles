27262af0fc6710e6eaa3c3dee89c3f85fcc270a7
Nikolaos Tourvas <nikostourvas@gmail.com>
2019-12-24 23:37:17 +0200

    add polybar launch script

--- 1.install_software.sh
+++ 1.install_software.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo add-apt-repository -y ppa:kgilmer/speed-ricer
 
-sudo apt install -y compton i3-gaps i3-gaps-wm-dbg polybar redshift-gtk nm-tray 
+sudo apt install -y compton i3-gaps i3-gaps-wm-dbg polybar redshift-gtk nm-tray rofi pavucontrol
 
 sudo dpkg-reconfigure fontconfig-config
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf && fc-cache
--- 2.install_script.sh
+++ 2.install_script.sh
@@ -5,6 +5,7 @@ ln -sv ~/dotfiles/zathurarc ~/.config/zathura
 ln -sv ~/dotfiles/redshift.conf ~/.config/
 mkdir ~/.config/polybar
 ln -sv ~/dotfiles/polybar/config ~/.config/polybar/
+ln -sv ~/dotfiles/polybar/launch.sh ~/.config/polybar/
 mkdir ~/.config/i3
 ln -sv ~/dotfiles/i3/config ~/.config/i3/
 ln -sv ~/dotfiles/compton/compton.conf ~/.config/compton.conf
