146c6a6bed5c3edf57fb00e4ee87eff56ed0b0f0
Nikolaos Tourvas <nikostourvas@gmail.com>
2019-12-25 01:14:40 +0200

    install additional software

--- 1.install_software.sh
+++ 1.install_software.sh
@@ -3,7 +3,41 @@
 sudo apt update
 sudo add-apt-repository -y ppa:kgilmer/speed-ricer
 
-sudo apt install -y compton i3-gaps i3-gaps-wm-dbg polybar redshift-gtk nm-tray rofi pavucontrol
+sudo apt install -y compton \
+	i3-gaps i3-gaps-wm-dbg \
+	polybar \
+	redshift-gtk \
+	nm-tray \
+	rofi \
+	pavucontrol \
+	caja \
+	lxappearance \
+	lm-sensors hddtemp \
+	p7zip p7zip-full p7zip-rar \
+	arandr \
+	qt5ct \
+	qbittorrent \
+	zathura \
+	vlc \
+	pdfarranger \
+	vim \
+	gimp \
+	virtualbox \
+	audacity 
 
+# Detect laptop sensors
+# Run manualy
+# sudo sensors-detect
+
+# Install Siji font
+git clone https://github.com/stark/siji && cd siji
+./install.sh
+
+# Install Nextwaita
+git clone https://github.com/paullinuxthemer/Nextwaita && cd Nextwaita
+cp -r . ~/.themes
+# open manualy lxappearance and set the theme and font size (Sans 7)
+
+# Setting for Siji font on Ubuntu and Debian systems
 sudo dpkg-reconfigure fontconfig-config
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf && fc-cache
---
+++ 3.install_additional_software.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install zotero manually
+
+sudo add-apt-repository ppa:saiarcot895/chromium-beta
+sudo apt update
+sudo apt install chromium-beta
+# check the website for instructions
+# https://launchpad.net/~saiarcot895/+archive/ubuntu/chromium-beta
+
+# Foxit Reader
+# Anki
+# viber
+# Scribus
+# Inkscape
+# Spotify
+# Dropbox
+# R-base repository & Rstudio
