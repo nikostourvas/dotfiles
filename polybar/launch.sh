be6fea7557577a002bd8bb52967c6e04607aceb7
nikostourvas <nikostourvas@gmail.com>
2020-05-16 02:51:44 +0300

    minor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,5 +9,4 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-#polybar trans -r & 
-polybar example -r & 
+polybar -c ~/.config/polybar/config.ini main &
