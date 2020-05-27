66bda1d4f2bc6bb9f863782e19e9d70e7ebc60b1
nikostourvas <nikostourvas@gmail.com>
2020-05-27 22:52:13 +0300

    minor

--- 2.install_script.sh
+++ 2.install_script.sh
@@ -9,3 +9,4 @@ ln -sv ~/dotfiles/polybar/launch.sh ~/.config/polybar/
 mkdir ~/.config/i3
 ln -sv ~/dotfiles/i3/config ~/.config/i3/
 ln -sv ~/dotfiles/compton/compton.conf ~/.config/compton.conf
+ln -sv ~/dotfiles/.Xresources ~/.Xresources
